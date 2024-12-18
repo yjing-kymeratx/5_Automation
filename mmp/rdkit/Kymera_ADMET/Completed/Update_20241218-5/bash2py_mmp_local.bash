3d402b856dbdda8d89424aaf6bd43b6765891b91
yjing <yjing@kymeratx.com>
2024-12-18 10:16:26 -0500

    2024Dec18

---
+++ mmp/rdkit/Kymera_ADMET/Completed/Update_20241218-5/bash2py_mmp_local.bash
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+
+
+
+## ===========================================
+## ============== Python SetUp ===============
+## ===========================================
+
+## 1) rm the existing global variable for python
+unset PYTHONPATH
+
+## 2) Initiate conda for python env
+#source /home/yjing/anaconda3/etc/profile.d/conda.sh
+source /home/yjing/data0/software/anaconda/anaconda3/etc/profile.d/conda.sh
+
+
+## 3) Activate custom conda environments
+conda activate /home/yjing/data0/software/anaconda/anaconda3/envs/mmp    ## 'ml', 'gpb39' ==> 'yjing'
+
+## 4) define global variables & import python packages?
+# export PatGlobalVars=/fsx/bin/PatGlobalVars.py
+# export yjingGlobalVars=/fsx/bin/yjingGlobalVars.py 
+
+#echo "You provided command line arguments:" "$@"
+#echo "You provided $# command line arguments"
+myArr=( "$@" )    ## put all the imported parameters in the list myArr
+myCmd=( "${myArr[@]:0:1}" )
+myArg=( "${myArr[@]:1}" )
+if [ -t 0 ]; then
+   #  nothing coming from stdin
+   #echo "Run the following: $@"
+   "${myCmd[@]}" "${myArg[@]}"
+else
+   # stuff is coming from stdin
+   #echo "Run the following: cat - | $@"
+   cat - | "${myCmd[@]}" "${myArg[@]}"
+fi
