404c4b1bbbcc8c5bad848dd702651c1a225dcd31
yjing <yjing@kymeratx.com>
2025-02-27 16:36:28 -0500

    2025Feb27

--- AutoML/bash_build_ML_models.bash
+++ AutoML/bash_build_ML_models.bash
@@ -19,92 +19,79 @@ echo "Today is $dateToday"
 # bash2py="./bash2py_yjing_local.bash"
 bash2py="/mnt/data0/Research/0_Test/cx_pKa/bash2py_yjing_local.bash"
 JobDir='./'
-resultDir="$JobDir/Results"
 
-## input 
+
+## S1: input 
 fileIn="$JobDir/Data/DataView_MDCK_MDR1__Permeability_1__export_top300.csv"
 sep=","
 colId='Compound Name'
 colSmi='Structure'
-
 colAssay='ADME MDCK(WT) Permeability;Mean;A to B Papp (10^-6 cm/s);(Num)'
 colAssayMod='ADME MDCK(WT) Permeability;Mean;A to B Papp (10^-6 cm/s);(Mod)'
 colDate="ADME MDCK(WT) Permeability;Concat;Run Date"    ## needed if 'temporal'
-
 colPreCalcDesc="ADME MDCK(WT) Permeability;Mean;A to B Recovery (%),ADME MDCK(WT) Permeability;Mean;B to A Papp (10^-6 cm/s);(Num)"
-colSplit="Split"
 
-## S1
-fileInS1=$fileIn
-fileOutS1="$resultDir/data_input_clean.csv"
-fileOutS1y="$resultDir/outcome_expt.csv"
-
-## S2
-fileInS2=$fileOutS1
-
+## S2: split params
 split='diverse'     #'random', 'diverse', 'temporal'
+colSplit="Split"
 CV=10
-rng=666666
 hasVal=True
-fileOutS2="$resultDir/data_split_$split.csv"
 
-## S3
-fileInS3=$fileOutS1
+## S3: calc descriptors
 desc_rdkit="True"
 desc_fps="True"
 desc_cx="True"
 norm="True"
 imput="True"
 
-fileOutS3="$resultDir/descriptors_prep_merged.csv"
-
-## S4
-fileInS4_X=$fileOutS3
-fileInS4_y=$fileOutS1y
-fileInS4_S=$fileOutS2
-
+## S4&5: feature sele & model building
 modelType="regression"    ## "regression" or "classification"
 nanFilter="True"
 VFilter="True"
 L2Filter="True"
 FIFilter="True"
 
-ImpuParamFile="$resultDir/feature_imputation_params.json"
-folderOutS4="$resultDir/data_input_4_ModelBuilding.csv"
-
-## S5
-fileInS5=$folderOutS4
-modelType=$modelType
 ml_linear="True"
 ml_rf="True"
 ml_svm="True"
 ml_mlp="True"
 ml_knn="True"
-
-rng=$rng
-njobs=-1
-logy="True"
 # doHPT="True"
 doHPT="False"
-folderOutS5="$resultDir/performance_results.csv"
 
-CalcParamFile="$resultDir/feature_calculator_param.json"
-NormParamFile="$resultDir/feature_normalization_params.json"
-ImpuParamFile=$ImpuParamFile
+## sys configs
+rng=666666
+njobs=-1
+logy="True"
+
+## output
+resultDir="$JobDir/Results"
+
+tmpFile_1_clean="$resultDir/data_input_clean.csv"
+tmpFile_1_y="$resultDir/outcome_expt.csv"
+tmpFile_2_split="$resultDir/data_split_$split.csv"
+tmpFile_3_desc="$resultDir/descriptors_prep_merged.csv"
+tmpFile_4_all4ml="$resultDir/data_input_4_ModelBuilding.csv"
+tmpFile_5_modelPerfm="$resultDir/performance_results.csv"
+
+tmpFile_CalcParam="$resultDir/feature_calculator_param.json"
+tmpFile_NormParam="$resultDir/feature_normalization_params.json"
+tmpFile_ImpuParam="$resultDir/feature_imputation_params.json"
+
 
 ## ------------------ step-1 csv loader & data clean ------------------
-$bash2py python "$JobDir"/DataPrep.py -i "$fileInS1" -d "$sep" --detectEncoding --colId "$colId" --colSmi "$colSmi" --colAssay "$colAssay" --colAssayMod "$colAssayMod" -o "$fileOutS1" --outputy "$fileOutS1y"
+$bash2py python "$JobDir"/DataPrep.py -i "$fileIn" -d "$sep" --detectEncoding --colId "$colId" --colSmi "$colSmi" --colAssay "$colAssay" --colAssayMod "$colAssayMod" -o "$tmpFile_1_clean" --outputy "$tmpFile_1_y"
 
 ## ------------------ step-2 train-val-test split ------------------
-$bash2py python "$JobDir"/DataSplit.py -i "$fileInS2" -d "$sep" --colId "$colId" --colSmi "$colSmi" --colDate "$colDate" --split "$split" --CV "$CV" --rng "$rng" --hasVal "$hasVal" --cols "$colSplit" -o "$fileOutS2"
+$bash2py python "$JobDir"/DataSplit.py -i "$tmpFile_1_clean" -d "$sep" --colId "$colId" --colSmi "$colSmi" --colDate "$colDate" --split "$split" --CV "$CV" --rng "$rng" --hasVal "$hasVal" --cols "$colSplit" -o "$tmpFile_2_split"
 
 ## ------------------ step-3 generate descriptors ------------------
-$bash2py python "$JobDir"/DescGen.py -i "$fileInS3" -d "$sep" --colId "$colId" --colSmi "$colSmi" --desc_rdkit "$desc_rdkit" --desc_fps "$desc_fps" --desc_cx "$desc_cx" --colPreCalcDesc "$colPreCalcDesc" --norm "$norm" --imput "$imput" -o "$fileOutS3"
+$bash2py python "$JobDir"/DescGen.py -i "$tmpFile_1_clean" -d "$sep" --colId "$colId" --colSmi "$colSmi" --desc_rdkit "$desc_rdkit" --desc_fps "$desc_fps" --desc_cx "$desc_cx" --colPreCalcDesc "$colPreCalcDesc" --norm "$norm" --imput "$imput" -o "$tmpFile_3_desc"
 
 ## ------------------ step-4 feature selection ------------------
-$bash2py python "$JobDir"/FeatSele.py -x "$fileInS4_X" -y "$fileInS4_y" -s "$fileInS4_S" -d "$sep" --colId "$colId" --coly "$colAssay" --cols "$colSplit" --modelType "$modelType" --MissingValueFilter "$nanFilter" --ImputationParamFile "$ImpuParamFile" --VarianceFilter "$VFilter" --L2Filter "$L2Filter" --FIFilter "$FIFilter" -o "$folderOutS4"
+$bash2py python "$JobDir"/FeatSele.py -x "$tmpFile_3_desc" -y "$tmpFile_1_y" -s "$tmpFile_2_split" -d "$sep" --colId "$colId" --coly "$colAssay" --cols "$colSplit" --modelType "$modelType" --MissingValueFilter "$nanFilter" --impuParamJson "$tmpFile_ImpuParam" --VarianceFilter "$VFilter" --L2Filter "$L2Filter" --FIFilter "$FIFilter" -o "$tmpFile_4_all4ml"
 
 ## ------------------ step-5 ML modeling ------------------
-$bash2py python "$JobDir"/ModelBuilding_r.py -i "$fileInS5" -d "$sep" --colId "$colId" --coly "$colAssay" --cols "$colSplit" --linear "$ml_linear" --rf "$ml_rf" --svm "$ml_svm" --mlp "$ml_mlp" --knn "$ml_knn" --njobs "$njobs" --rng "$rng" --logy $logy --doHPT "$doHPT" --calcParamJson "$CalcParamFile" --normParamJson "$NormParamFile" --impuParamJson "$ImpuParamFile" -o "$folderOutS5"
+$bash2py python "$JobDir"/ModelBuilding_r.py -i "$tmpFile_4_all4ml" -d "$sep" --colId "$colId" --coly "$colAssay" --cols "$colSplit" --linear "$ml_linear" --rf "$ml_rf" --svm "$ml_svm" --mlp "$ml_mlp" --knn "$ml_knn" --njobs "$njobs" --rng "$rng" --logy $logy --doHPT "$doHPT" --calcParamJson "$tmpFile_CalcParam" --normParamJson "$tmpFile_NormParam" --impuParamJson "$tmpFile_ImpuParam" -o "$tmpFile_5_modelPerfm"
 
 ## ------------------ step-6 to be added ------------------
